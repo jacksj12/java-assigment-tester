0cb62a9e11d66a44e8e6ad405629588d90f8c93e
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2018-09-04 20:47:30 -0400

    initial commit with working (failing checks) sample lab -- expecting a failed Travis CI build

---
+++ src/main/java/samplelab/DataClass.java
@@ -0,0 +1,24 @@
+package samplelab;
+
+import java.util.Date;
+
+/**
+ * This is a sample data class.
+ *
+ * @author Saejin Mahlau-Heinert
+ */
+public class DataClass {
+  int value;
+
+  public DataClass(int value) {
+    this.value = value;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+}
---
+++ src/main/java/samplelab/SampleLabMain.java
@@ -0,0 +1,21 @@
+package samplelab;
+
+import java.util.Date;
+
+/**
+ * This is a sample lab to be graded with GatorGrader.
+ *
+ * @author Saejin Mahlau-Heinert
+ */
+public class SampleLabMain {
+  /**
+   * This is the main method that calls all of the necessary methods.
+   *
+   * @author Saejin Mahlau-Heinert
+   */
+  public static void main(String[] args) {
+    // Step 1: Display the welcome message
+    System.out.println("Hello World! DATA: " + new DataClass(4));
+    System.out.println("GatorGradle " + new Date());
+  }
+}
